58b449ef15371d9c5fc1eabf0f6bae37f8bede4b
Jon William Taylor <jwtaylor659@gmail.com>
2023-11-22 13:33:13 -0600

    Second/improved version of SimpleImageClassifier

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <header>
+      <h1>Image Classifier App</h1>
+    </header>
+    <main>
+      <SimpleImageClassifierv2 />
+    </main>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SimpleImageClassifierv2 from './components/SimpleImageClassifierv2.vue';
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    SimpleImageClassifierv2,
+  },
+};
+</script>
